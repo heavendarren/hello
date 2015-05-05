83ac2250f692c6c73f43364129dcde4adb6fae71
mygit <heavendarren@126.com>
2015-05-05 09:14:50 +0800

    Initial commit

---
+++ untitled/src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2015/4/28.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("this is my baby");
+        System.out.println();
+    }
+}
